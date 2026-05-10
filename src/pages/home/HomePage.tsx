43a357622b1488b76cbf0e513f814697e9bfeb9e
Tnalxmsk <alstnwkd990@naver.com>
2026-05-11 02:14:12 +0900

    refactor: 홈 페이지 컴포넌트 원복 (#19)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,3 @@
-import {SearchBar} from './components/search-bar/SearchBar';
-
 export const HomePage = () => {
-  return (
-    <main className='min-h-dvh bg-white px-[18px] py-[25px]'>
-      <SearchBar />
-    </main>
-  );
+  return <h1>Home</h1>;
 };
