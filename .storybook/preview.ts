7afb3c575552c1eabfce9bf45724b8d52d87a480
winchoose <tmdxor741@naver.com>
2026-05-11 00:08:18 +0900

    feat: 파일 수정 (#35)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,12 @@
 import type {Preview} from '@storybook/react-vite';
+import {createElement} from 'react';
+import {QueryProvider} from '../src/app/providers/QueryProvider';
 import '../src/shared/styles/global.css';
 
 const preview: Preview = {
+  decorators: [
+    (Story) => createElement(QueryProvider, null, createElement(Story)),
+  ],
   parameters: {
     actions: {argTypesRegex: '^on[A-Z].*'},
 
