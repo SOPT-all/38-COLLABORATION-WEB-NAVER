10f934d4386c45970a454246e73a886c4c9faca3
winchoose <tmdxor741@naver.com>
2026-05-10 20:02:04 +0900

    feat: svg 모두 추가 후 변환 (#29)

---
+++ src/shared/icons/ic_map.tsx
@@ -0,0 +1,14 @@
+import type {SVGProps} from 'react';
+const IcSvgMap = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    {...props}>
+    <path
+      fill='currentColor'
+      d='M12 4c3.314 0 6 2.904 6 6.485a6.78 6.78 0 0 1-1.418 4.185q-.062.151-.162.29l-3.293 4.472a1.4 1.4 0 0 1-2.254 0L7.58 14.96a1.4 1.4 0 0 1-.163-.291A6.78 6.78 0 0 1 6 10.485C6 6.904 8.686 4 12 4m0 4.39c-.884 0-1.6.715-1.6 1.596s.716 1.597 1.6 1.597 1.6-.715 1.6-1.597c0-.881-.716-1.596-1.6-1.596'
+    />
+  </svg>
+);
+export default IcSvgMap;
