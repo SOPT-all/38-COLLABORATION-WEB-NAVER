b91a4ecd430eef4fcac1578b4e65e986b83c35e5
ssolsh <sohee220075@gmail.com>
2026-05-12 00:31:43 +0900

    style: 상품 이미지 배지 포맷팅 적용(#53)

--- src/pages/product/components/product-image-section/ProductMembershipBadge.tsx
+++ src/pages/product/components/product-image-section/ProductMembershipBadge.tsx
@@ -4,10 +4,10 @@ type ProductMembershipBadgeProps = {
 
 export function ProductMembershipBadge({text}: ProductMembershipBadgeProps) {
   return (
-    <div className='flex h-[2.2rem] w-[15.2rem] items-center rounded-[2.3rem] bg-gray-800 p-[0.2rem] text-caption-12sb text-white'>
+    <div className='text-caption-12sb flex h-[2.2rem] w-[15.2rem] items-center rounded-[2.3rem] bg-gray-800 p-[0.2rem] text-white'>
       <div className='flex h-[1.8rem] w-[14.6rem] items-center gap-[0.2rem]'>
         <span className='size-[1.8rem] shrink-0 rounded-full bg-red-700' />
-        <span className='relative top-[0.1rem] block h-[1.4rem] w-[12.6rem] whitespace-nowrap leading-[100%] tracking-[0] text-white'>
+        <span className='relative top-[0.1rem] block h-[1.4rem] w-[12.6rem] leading-[100%] tracking-[0] whitespace-nowrap text-white'>
           {text}
         </span>
       </div>
