b8918f9c408d5d7fa7e58aceeeffde0134f167e7
hdg0116 <hdg0116@gmail.com>
2026-05-11 14:17:16 +0900

    feat: 누락된 아이콘 파일 추가 및 tag-pay 아이콘 추가 (#47)

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -24,3 +24,4 @@ export {default as IcSvgQuestion} from './ic-question';
 export {default as IcSvgSearch} from './ic-search';
 export {default as IcSvgStar} from './ic-star';
 export {default as IcSvgTagDelivery} from './ic-tag-delivery';
+export {default as IcSvgTagPay} from './ic-tag-pay';
