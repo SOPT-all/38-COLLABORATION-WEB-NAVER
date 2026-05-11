9240ef779e25d3ef2ac2571efd36f1e227f6130d
winchoose <tmdxor741@naver.com>
2026-05-11 16:41:05 +0900

    feat: 구조잡고 공컴 대기 (#50)

--- src/pages/cart/components/address-box/AddressBox.tsx
+++ src/pages/cart/components/address-box/AddressBox.tsx
@@ -1,12 +1,18 @@
 import {IcSvgMap} from '@/shared/icons';
 
-export const AddressBox = () => {
+type AddressBoxProps = {
+  address: string;
+  onChangeAddress: () => void;
+};
+
+export const AddressBox = ({address, onChangeAddress}: AddressBoxProps) => {
   return (
     <div>
       <div>
         <IcSvgMap width={24} height={24} />
-        <p>서울 마포구 백범로31길</p>
+        <p>{address}</p>
       </div>
+      <button onClick={onChangeAddress}>공컴머지하면바꾸자</button>
     </div>
   );
 };
