c13da7a34c15e0673c83965c3999d6900fb7b9a0
kimminna <kmina121777@naver.com>
2026-05-11 16:38:44 +0900

    fix(style): 잘못된 폰트 클래스 속성 수정 (#43)

--- src/shared/components/button/OptionButton.tsx
+++ src/shared/components/button/OptionButton.tsx
@@ -12,7 +12,7 @@ export const OptionButton = ({
     <button
       type='button'
       className={cn(
-        'text-body-m w-73.25 rounded-sm border border-gray-500 py-2.25 text-gray-900',
+        'text-body-14m w-73.25 rounded-sm border border-gray-500 py-2.25 text-gray-900',
         className
       )}
       {...props}>
