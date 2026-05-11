7c1ed09abf09c77f442206a5e9c1757f7f4673ed
winchoose <tmdxor741@naver.com>
2026-05-11 23:45:07 +0900

    fix: 스타일 수정 (#50)

--- src/pages/cart/components/address-box/AddressBox.tsx
+++ src/pages/cart/components/address-box/AddressBox.tsx
@@ -9,8 +9,8 @@ type AddressBoxProps = {
 
 export const AddressBox = ({address, onChangeAddress}: AddressBoxProps) => {
   return (
-    <div className='text-caption-12m flex justify-between border-b-2 border-gray-500 px-4 py-2'>
-      <div className='flex items-center gap-1'>
+    <div className='text-caption-12m flex justify-between border-b-2 border-gray-500 px-[16px] py-[8px]'>
+      <div className='flex items-center gap-[4px]'>
         <IcSvgMap width={24} height={24} color='var(--color-green-600' />
         <p className='text'>{address}</p>
       </div>
