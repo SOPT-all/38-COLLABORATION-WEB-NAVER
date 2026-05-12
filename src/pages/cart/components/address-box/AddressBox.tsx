bc7102cdfe15fb2834ec9fe84514442109055865
winchoose <tmdxor741@naver.com>
2026-05-12 23:20:04 +0900

    feat: 리뷰 사항 수정 (#50)

--- src/pages/cart/components/address-box/AddressBox.tsx
+++ src/pages/cart/components/address-box/AddressBox.tsx
@@ -1,4 +1,3 @@
-import {CHANGE_ADDRESS} from '@/pages/cart/constants/CartConstants';
 import {ActionButton} from '@/shared/components/button/ActionButton';
 import {IcSvgMap} from '@/shared/icons';
 
@@ -11,11 +10,19 @@ export const AddressBox = ({address, onChangeAddress}: AddressBoxProps) => {
   return (
     <div className='text-caption-12m flex justify-between border-b-2 border-gray-500 px-[16px] py-[8px]'>
       <div className='flex items-center gap-[4px]'>
-        <IcSvgMap width={24} height={24} color='var(--color-green-600' />
-        <p className='text'>{address}</p>
+        <IcSvgMap
+          width={24}
+          height={24}
+          aria-hidden='true'
+          color='var(--color-green-600)'
+        />
+        <p className='truncate'>{address}</p>
       </div>
-      <ActionButton variant='sm' onClick={onChangeAddress}>
-        {CHANGE_ADDRESS}
+      <ActionButton
+        variant='sm'
+        aria-label='배송지 변경'
+        onClick={onChangeAddress}>
+        변경
       </ActionButton>
     </div>
   );
--- src/pages/cart/constants/CartConstants.ts
+++ src/pages/cart/constants/CartConstants.ts
@@ -1,3 +1,2 @@
 export const NORMAL_DELIVERY_LABEL = '일반배송';
 export const KURLY_DELIVERY_LABEL = '컬리N마트 · 지금배달';
-export const CHANGE_ADDRESS = '변경';
