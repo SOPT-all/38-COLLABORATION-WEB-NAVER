e955dc8d339e4619c7f5faf6479afd03584b55e8
hdg0116 <hdg0116@gmail.com>
2026-05-15 02:01:35 +0900

    feat: 결제 페이지 사용 아이콘 추가 (#77)

---
+++ src/shared/icons/ic-check-lg-checked.tsx
@@ -0,0 +1,15 @@
+import type {SVGProps} from 'react';
+const IcSvgCheckLgChecked = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    {...props}>
+    <rect width={20} height={20} x={2} y={2} fill='#1AAB52' rx={10} />
+    <path
+      fill='currentColor'
+      d='M16.156 8.738a.5.5 0 0 1 .688.726l-5.995 5.662a1 1 0 0 1-1.405-.03l-2.302-2.374a.5.5 0 0 1 .716-.696l2.304 2.373z'
+    />
+  </svg>
+);
+export default IcSvgCheckLgChecked;
