cfffa284987c5a78b38d99ff227b26e4a49a1a97
hdg0116 <hdg0116@gmail.com>
2026-05-16 00:54:40 +0900

    feat: 헤더의 X 아이콘 클릭 시 장바구니 페이지로 이동 기능 추가 (#97)

--- src/pages/order-sheet/OrderSheetPage.tsx
+++ src/pages/order-sheet/OrderSheetPage.tsx
@@ -1,3 +1,5 @@
+import {useNavigate} from 'react-router-dom';
+
 import {OrderSheetHeader} from '@/pages/order-sheet/components/order-sheet-header/OrderSheetHeader';
 import {
   MOCK_ORDER_SHEET_RESPONSE_DATA,
@@ -5,6 +7,7 @@ import {
   MOCK_REWARD_INFO,
   MOCK_REWARD_ITEMS,
 } from '@/pages/order-sheet/mocks/RewardInfoCardMocks';
+import {ROUTES} from '@/shared/constants/routes';
 
 import {AgreementSection} from '@/pages/order-sheet/sections/agreement-section/AgreementSection';
 import {PaymentButtonSection} from '@/pages/order-sheet/sections/payment-button-section/PaymentButtonSection';
@@ -12,9 +15,18 @@ import {PointMoneySection} from '@/pages/order-sheet/sections/point-money-sectio
 import {RewardPointSection} from '@/pages/order-sheet/sections/reward-point-section/RewardPointSection';
 
 export function OrderSheetPage() {
+  const navigate = useNavigate();
+
+  const handleCloseClick = () => {
+    navigate(ROUTES.CART);
+  };
+
   return (
     <div className='flex min-h-screen flex-col gap-[31px] bg-gray-300'>
-      <OrderSheetHeader className='pt-[72px] pr-[16px] pb-[12px] pl-[16px]' />
+      <OrderSheetHeader
+        className='pt-[72px] pr-[16px] pb-[12px] pl-[16px]'
+        onCloseClick={handleCloseClick}
+      />
 
       <main className='flex flex-col gap-[40px] px-[16px] pb-[16px]'>
         <PointMoneySection {...MOCK_POINT_MONEY_DATA} />
