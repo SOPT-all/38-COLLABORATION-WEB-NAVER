4ef001a711e91aebb81f735d6f325104b734f78f
hdg0116 <hdg0116@gmail.com>
2026-05-16 00:43:12 +0900

    feat: 결제 페이지 헤더 및 컴포넌트 영역 레이아웃 세팅 (#97)

--- src/pages/order-sheet/OrderSheetPage.tsx
+++ src/pages/order-sheet/OrderSheetPage.tsx
@@ -1,3 +1,13 @@
+import { OrderSheetHeader } from "@/pages/order-sheet/components/order-sheet-header/OrderSheetHeader";
+
 export function OrderSheetPage() {
-  return <h1>Order Sheet</h1>;
+  return (
+    <div className='flex min-h-screen flex-col gap-[31px] bg-gray-300'>
+      <OrderSheetHeader className='pt-[72px] pr-[16px] pb-[12px] pl-[16px]' />
+
+      <main className='flex flex-col gap-[40px] px-[16px] pb-[16px]'>
+        컴포넌트 영역
+      </main>
+    </div>
+  );
 }
